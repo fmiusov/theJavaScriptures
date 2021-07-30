f1d7c4d2eb8e693508b766238496643de4939043
Jayson Duff <jayson.duff@gsgus.com>
2021-07-29 20:32:51 -0400

    Adding Array of method

--- arraysAndIterables/app.js
+++ arraysAndIterables/app.js
@@ -4,8 +4,8 @@ console.log(numbers);
 // const moreNumbers = Array(5, 2);
 // console.log(moreNumbers);
 
-// const yetMoreNumbers = Array.of(1, 2);
-// console.log(yetMoreNumbers);
+const yetMoreNumbers = Array.of(1, 2);
+console.log(yetMoreNumbers);
 
 const listItems = document.querySelectorAll('li');
 console.log(listItems);
